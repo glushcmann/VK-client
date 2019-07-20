0fe3084cdab984240dd37a0abc9ec487ee9d01f9
Nick <nikita.glu@icloud.com>
2019-07-20 12:42:28 +0300

    Initial Commit

---
+++ VK client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK client
+//
+//  Created by Никита on 20/07/2019.
+//  Copyright © 2019 Nikita Glushchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
